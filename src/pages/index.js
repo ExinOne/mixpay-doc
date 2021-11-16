443dcffe7d17acb7bc0808c06da977b246b7c454
Robin Wen <blockxyz@gmail.com>
2021-11-16 21:41:42 +0800

    Update title

--- src/pages/index.js
+++ src/pages/index.js
@@ -72,8 +72,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="MixPay is a decentralized instant payment platform built on the Mixin Network.">
       <HomepageHeader />
       <main>
         {/* <HomepageFeatures /> */}
